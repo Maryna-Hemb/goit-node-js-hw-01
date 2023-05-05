d9dd0a2e0648bfbe89e02020c96a5005eb05a24f
Maryna-Hemb <marinagembarskaa@gmail.com>
2023-05-05 21:10:34 +0300

    delete brake

--- index.js
+++ index.js
@@ -7,22 +7,18 @@ async function invokeAction({ action, id, name, email, phone }) {
       case "list":
         const allContacts = await contacts.listContacts();
         return console.log(allContacts);
-        break;
 
       case "get":
         const oneContact = await contacts.getContactById(id);
         return console.log(oneContact);
-        break;
 
       case "add":
         const newContact = await contacts.addContact(name, email, phone);
         return console.log(newContact);
-        break;
 
       case "remove":
         const deletContact = await contacts.removeContact(id);
         return console.log(deletContact);
-        break;
 
       default:
         console.warn("\x1B[31m Unknown action type!");
